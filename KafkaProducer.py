6693211c5b1719056bacf4c9d571d6f5edcee6bb
undadasea <valeriatazh@gmail.com>
2019-10-29 01:44:35 +0300

    key value formatting

--- KafkaProducer.py
+++ KafkaProducer.py
@@ -3,13 +3,28 @@ from time import gmtime, strftime
 from kafka import KafkaProducer
 
 import socket
-import pye
 import struct
+
+# this module captures traffic on all interfaces and sends information
+# about packets to the kafka topic
+
+# runs with python KafkaProducer.py (after all packages installed)
+
+def get_str_bytes_ip(ip):
+    addr_array = ip.split(".")
+    for i in range(4):
+        addr_array[i] = "{0:#0{1}x}".format(int(addr_array[i]),4)
+
+    return "\\"+addr_array[0]+"\\"+ \
+                addr_array[1]+"\\"+ \
+                addr_array[2]+"\\"+ \
+                addr_array[3]
 
 def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
+        print("key_bytes: ", key_bytes)
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
         producer_instance.flush()
         print("Message '{0}' published successfully.".format(value))
@@ -37,9 +52,10 @@ def main():
       source_ip = socket.inet_ntoa(ip_header[8])
       destination_ip = socket.inet_ntoa(ip_header[9])
       total_length = ip_header[2]
-      publish_message(kafka_producer, 'new_topic', source_ip+destination_ip, str(total_length))
+      publish_message(kafka_producer, 'new_topic', get_str_bytes_ip(source_ip)+ \
+            get_str_bytes_ip(destination_ip), str(total_length))
       time.sleep(10)
-      
+
 
 if __name__ == '__main__':
     main()
