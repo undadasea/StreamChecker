3ca23d461338b61ff0585e96bf54bdd6fda3024b
undadasea <valeriatazh@gmail.com>
2019-10-29 05:11:18 +0300

    jdbc is replaced by sql.streaming from json

--- DBReader.py
+++ DBReader.py
@@ -1,4 +1,5 @@
 import pyspark.sql
+from pyspark.sql.types import *
 
 #url = jdbc:postgresql://localhost:5432/<db_name>
 def readDB(spark):
@@ -13,4 +14,22 @@ def readDB(spark):
             .load()
 
     dataframe.show()
-    return dataframe
+    dataframe.registerTempTable("df_table")
+    limits = spark.sql("SELECT limit_name, limit_value, MAX(effective_date) as last_date "+ \
+                        "FROM df_table GROUP BY limit_name, limit_value")
+
+    log_file = open('./log_file', 'a')
+    log_file.write("Limits type: " + str(type(limits)))
+    #log_file.write("Min: " + str(limits.limit_value['Minimum'])
+    log_file.close()
+
+    return limits
+
+def createDBstream(spark):
+    schema = StructType([ StructField("limit_name", StringType(), True),
+                          StructField("limit_value", IntegerType(), True),
+                          StructField("effective_date", TimestampType(), True) ])
+
+    streamingDF = (spark.readStream.schema(schema).option("maxFilesPerTrigger", 1) \
+                        .json("/home/undadasea/StreamChecker/traffic_limits.json"))
+    return streamingDF
